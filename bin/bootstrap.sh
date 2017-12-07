3f0d50a9e2b675c0b7b3d2315ce743347c60b840
bebebeko <m3tac0ma@gmail.com>
2017-12-07 10:46:50 +0400

    setting upstream repo for publishing

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,7 +3,7 @@ set -xe -o pipefail
 
 # XXX this should break the deployment on amazon
 BOOTSTRAP_REPO="https://github.com/metacoma/aws-minikube.git"
-BOOTSTRAP_REPO="http://localhost:10080/bebebeko/k8spray.git"
+#BOOTSTRAP_REPO="http://localhost:10080/bebebeko/k8spray.git"
 TMP_DIR=`mktemp -u /tmp/minikube-bootstrap.XXXXXXXX`
 
 env
