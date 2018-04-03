ed979d9d17a0575ba8cf6d3b1b1017b8d14073fe
None <m3tac0ma@gmail.com>
2018-04-03 23:14:25 +0400

    change upstream repo

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,7 +2,7 @@
 set -xe -o pipefail
 
 # XXX this should break the deployment on amazon
-BOOTSTRAP_REPO="https://github.com/metacoma/aws-minikube.git"
+BOOTSTRAP_REPO="https://github.com/smashware/spinnaker-minikube.git"
 #BOOTSTRAP_REPO="http://localhost:10080/bebebeko/k8spray.git"
 TMP_DIR=`mktemp -u /tmp/minikube-bootstrap.XXXXXXXX`
 
